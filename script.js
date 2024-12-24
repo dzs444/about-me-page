c653d6a6feb10c91730eee013d337aa35f026799
dzs444 <addinna@ur.unirazak.edu.my>
2024-12-24 12:39:42 +0800

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('greetingBtn').addEventListener('click', function() {
     const userName = prompt("What's your name?");
     const greetingDiv = document.getElementById('greeting');
-    greetingDiv.textContent = `Hello, ${userName}! Welcome to my page!`;
+    greetingDiv.textContent = `Hello, ${Sir Zamil!}! Welcome to my page!`;
     greetingDiv.classList.remove('hidden');
     setTimeout(() => {
         greetingDiv.style.opacity = 1;
