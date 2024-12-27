782797149ec104b3cc87a3d432e49be03af372e8
dzs444 <addinna@ur.unirazak.edu.my>
2024-12-27 12:52:49 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('greetingBtn').addEventListener('click', function() {
-    const userName = prompt("What's your name?");
-    const greetingDiv = document.getElementById('greeting');
-    greetingDiv.textContent = `Hello, ${Sir Zamil!}! Welcome to my page!`;
-    greetingDiv.classList.remove('hidden');
-    setTimeout(() => {
-        greetingDiv.style.opacity = 1;
-    }, 100);
-});
